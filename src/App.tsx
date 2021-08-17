1c11155998ff2e21107190aeffe8af356ead6041
eugeneprib <yevgenyprib@gmail.com>
2021-08-17 19:59:13 +0300

    Changed eslint rule

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import styles from 'src/styles/globalStyles'
 import store from './store'
 
 const App = (): React.ReactElement => {
-  console.log()
-
   return (
     <Provider store={store}>
       <SafeAreaView style={styles.container}>
