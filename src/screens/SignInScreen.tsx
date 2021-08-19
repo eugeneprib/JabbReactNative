78b3d99b5bf7d33ce21d2a093d2b6155821a6218
Vitaliy <270vitalik270@gmail.com>
2021-08-19 22:49:26 +0300

    J-211 + mobile-authentication-screen

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
-import { TouchableOpacity, StyleSheet, View, Text, TextInput, Button, ScrollView } from 'react-native'
-import { StackNavigationProp } from '@react-navigation/stack';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView
+} from 'react-native'
+import { StackNavigationProp } from '@react-navigation/stack'
 
 type RootStackParamList = {
-  Home: undefined;
-  SignUpScreen: undefined;
-  SignIn: undefined;
-};
+  Home: undefined
+  SignUpScreen: undefined
+  SignIn: undefined
+}
 
 type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'SignIn'
->;
+>
 
 type Props = {
-  navigation: LoginScreenNavigationProp;
-};
+  navigation: LoginScreenNavigationProp
+}
 
 const LoginScreen: React.FC<Props> = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   const handleSignInSubmit = (): void => {
-    navigation.replace('Home');
-  };
+    navigation.replace('Home')
+  }
 
   return (
     <ScrollView>
@@ -34,19 +42,19 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
           <Text style={styles.lets}>Let&apos;s sign you in</Text>
           <TextInput
             style={styles.input}
-            placeholder="email"
-            returnKeyType="next"
+            placeholder='email'
+            returnKeyType='next'
             value={email}
             onChangeText={(text: string) => setEmail(text)}
-            autoCapitalize="none"
-            autoCompleteType="email"
-            textContentType="emailAddress"
-            keyboardType="email-address"
+            autoCapitalize='none'
+            autoCompleteType='email'
+            textContentType='emailAddress'
+            keyboardType='email-address'
           />
           <TextInput
             style={styles.input}
-            placeholder="password"
-            returnKeyType="done"
+            placeholder='password'
+            returnKeyType='done'
             value={password}
             onChangeText={(text: string) => setPassword(text)}
             secureTextEntry
@@ -55,7 +63,9 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
         <View style={styles.bottomBlock}>
           <View style={styles.row}>
             <Text>Don’t have an account? </Text>
-            <TouchableOpacity onPress={() => navigation.replace('SignUpScreen')}>
+            <TouchableOpacity
+              onPress={() => navigation.replace('SignUpScreen')}
+            >
               <Text style={styles.link}>Sign up</Text>
             </TouchableOpacity>
           </View>
@@ -73,41 +83,41 @@ const styles = StyleSheet.create({
     marginBottom: 50,
     marginLeft: 25,
     marginRight: 25,
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   hello: {
     fontSize: 36,
-    fontWeight: '700',
+    fontWeight: '700'
   },
   intro: {
-    fontSize: 18,
+    fontSize: 18
   },
   lets: {
     fontSize: 18,
-    marginBottom: 40,
+    marginBottom: 40
   },
   input: {
     borderWidth: 1,
     borderColor: '#EBEBEB',
     borderRadius: 4,
-    marginBottom: 15,
+    marginBottom: 15
   },
   bottomBlock: {
-    flex:1,
+    flex: 1,
     flexDirection: 'column',
-    marginTop: 25,
+    marginTop: 25
   },
   row: {
     flexDirection: 'row',
     marginTop: 4,
-    marginBottom: 10,
+    marginBottom: 10
   },
   forgot: {
-    fontSize: 13,
+    fontSize: 13
   },
   link: {
-    fontWeight: 'bold',
-  },
+    fontWeight: 'bold'
+  }
 })
 
-export default LoginScreen;
+export default LoginScreen
