dceddb516938fdcce5db1aa0604141e0db914514
eugeneprib <yevgenyprib@gmail.com>
2021-08-19 12:15:01 +0300

    Added basic navigation plus example setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 import styles from 'src/styles/globalStyles'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import store from './store'
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Text style={styles.greeting}>Hello there. This is the App</Text>
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
 
 const App = (): React.ReactElement => {
   return (
-    <Provider store={store}>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.greeting}>Hello there. This is the App</Text>
-      </SafeAreaView>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <SafeAreaView style={styles.container}>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </SafeAreaView>
+      </Provider>
+    </NavigationContainer>
   )
 }
 
