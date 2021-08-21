ed6e3530cc8d8a5962bba05d71413b84a1090d89
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-21 20:48:43 +0300

    J-210: + types to 'Heading' constants

--- src/components/heading/constants/index.ts
+++ src/components/heading/constants/index.ts
@@ -1,6 +1,7 @@
+import { StyleProp, TextStyle } from 'react-native'
 import { HeadingType } from 'src/common/enums'
 
-const DEFAULT_STYLES = {}
-const DEFAULT_TYPE = HeadingType.MEDIUM
+const DEFAULT_STYLES: StyleProp<TextStyle> = {}
+const DEFAULT_TYPE: HeadingType = HeadingType.MEDIUM
 
 export { DEFAULT_STYLES, DEFAULT_TYPE }
