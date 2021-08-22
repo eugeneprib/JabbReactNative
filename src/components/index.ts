82c671ee5e3e958ba92fa3eadcffca6e46da208b
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-22 22:41:53 +0300

    J-209: * 'index.ts' in src/components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-export { default as Button } from './button'
-export { default as Input } from './input'
-export { default as Link } from './link'
+export { default as Button, ButtonType } from './Button'
+export { default as Input } from './Input'
+export { default as Link } from './Link'
