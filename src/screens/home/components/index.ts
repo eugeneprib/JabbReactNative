a9984011945fa384e009a7c34ff4dd25277cd3b1
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-25 18:55:16 +0300

    J-244: + 'Home' screen

---
+++ src/screens/home/components/index.ts
@@ -0,0 +1,4 @@
+export { default as PopularSingleCard } from './popularSingleCard/'
+export { default as RecentlyPlayedCard } from './recentlyPlayedCard'
+export { default as UserAvatar } from './userAvatar'
+export { default as SuggestedPodcastCarousel } from './suggestedPodcastCarousel'
