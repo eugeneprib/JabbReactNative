9e6a0c0a2e12a75eeac3538dac6d104007f74566
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-26 13:35:22 +0300

    J-244: * 'App' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import { Provider } from 'react-redux'
-import { SafeAreaView, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import Toast from 'react-native-toast-message'
 import styles from 'src/styles/globalStyles'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { Heading } from 'src/components'
+import { Home } from './screens'
 import store from './store'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Heading label="Home Screen" style={{ marginBottom: 5 }} />
-      <Text style={styles.greeting}>Hello there. This is the App!!!</Text>
-    </View>
-  )
-}
 
 const Stack = createStackNavigator()
 
@@ -26,7 +17,14 @@ const App = (): React.ReactElement => {
       <Provider store={store}>
         <SafeAreaView style={styles.container}>
           <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerShown: false,
+                cardStyle: { backgroundColor: '#fff' }
+              }}
+            />
           </Stack.Navigator>
           <Toast ref={(ref) => Toast.setRef(ref)} />
         </SafeAreaView>
