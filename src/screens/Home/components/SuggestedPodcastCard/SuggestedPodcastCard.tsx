717ae0ed621acafbed46589d66903df875ee8eee
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-26 13:32:15 +0300

    J-244: + 'SuggestedPodcastCard' component

---
+++ src/screens/Home/components/SuggestedPodcastCard/SuggestedPodcastCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Heading, HeadingType } from 'src/components'
+import PlayIcon from 'src/assets/images/play.svg'
+import styles from './styles'
+
+type Props = {
+  title: string
+  author: string
+  source?: string
+  onPress?: () => void
+}
+
+const SuggestedPodcastCard: React.FC<Props> = ({
+  title,
+  author,
+  source,
+  onPress
+}) => {
+  return (
+    <ImageBackground source={{ uri: source }} style={styles.container}>
+      <View style={styles.overlay}>
+        <Text style={styles.author}>{author}</Text>
+        <View style={styles.description}>
+          <Heading
+            label={title}
+            type={HeadingType.LARGE}
+            style={styles.title}
+            numberOfLines={2}
+          />
+          <TouchableOpacity
+            style={styles.icon}
+            activeOpacity={0.85}
+            onPress={onPress}
+          >
+            <PlayIcon width={12} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  )
+}
+
+export default SuggestedPodcastCard
