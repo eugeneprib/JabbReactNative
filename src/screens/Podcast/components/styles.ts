7e4ee88371234128b4d9c87e63624b67dad6b688
Yevhenii <eucheremisin@gmail.com>
2021-08-26 19:11:35 +0300

    J-274: *styles

--- src/screens/Podcast/components/styles.ts
+++ src/screens/Podcast/components/styles.ts
@@ -1,39 +1,38 @@
-import { StyleSheet } from "react-native";
-
+import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        paddingVertical: 5,
-        marginBottom: 10,
-        height: 50,
-    },
-    episodeNumberCont:{
-        flex: 0.1,
-        width: 20,
-    },
-    episodeNumber: {
-        textAlign: 'center',
-        fontSize: 12,
-        fontStyle: 'italic',
-    },
-    episodeInfo: {
-        flex: 0.9,
-        paddingHorizontal: 30,
-        justifyContent: 'space-around',
-    },
-    episodeInfoTitle:{
-        fontWeight: 'bold',
-        fontSize: 16,
-        width: 200,
-    },
-    episodeInfoDate: {
-        fontSize: 10,
-        marginTop: 10,
-        color: '#555',
-    },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingVertical: 5,
+    marginBottom: 10,
+    height: 50
+  },
+  episodeNumberCont: {
+    flex: 0.1,
+    width: 20
+  },
+  episodeNumber: {
+    textAlign: 'center',
+    fontSize: 12,
+    fontStyle: 'italic'
+  },
+  episodeInfo: {
+    flex: 0.9,
+    paddingHorizontal: 30,
+    justifyContent: 'space-around'
+  },
+  episodeInfoTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    width: 200
+  },
+  episodeInfoDate: {
+    fontSize: 10,
+    marginTop: 10,
+    color: '#555'
+  }
 })
 
-export default styles;
+export default styles
