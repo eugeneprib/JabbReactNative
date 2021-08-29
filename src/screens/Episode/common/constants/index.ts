059412772261e5b7ffaf52029c59f4f47272fc89
Vitaliy <270vitalik270@gmail.com>
2021-08-29 12:56:44 +0300

    J-279 * did changes in getTime helper

--- src/screens/Episode/common/constants/index.ts
+++ src/screens/Episode/common/constants/index.ts
@@ -5,10 +5,12 @@ const DEFAULT_EPISODE_TRACK =
   'https://www.chosic.com/wp-content/uploads/2021/07/The-Epic-Hero-Epic-Cinematic-Keys-of-Moon-Music.mp3'
 
 const DEFAULT_START_TIME = 0
+const UNIX_TIME_START = 0
 
 export {
   TIME_SHIFT_IN_SECONDS,
   DEFAULT_EPISODE_IMAGE,
   DEFAULT_EPISODE_TRACK,
-  DEFAULT_START_TIME
+  DEFAULT_START_TIME,
+  UNIX_TIME_START
 }
--- src/screens/Episode/helpers/getTime.ts
+++ src/screens/Episode/helpers/getTime.ts
@@ -1,17 +1,28 @@
 import * as dateFns from 'date-fns'
+import { UNIX_TIME_START } from '../common'
 import { DateFormatType, getFormattedDate } from 'src/helpers'
 
-const getTime = (seconds: number): string => {
+const checkHourExist = (hours: string): boolean => {
+  return hours === '00' ? false : true
+}
+
+const getTime = (secondsAsNumber: number): string => {
   const timeInFormatHHmmss = getFormattedDate(
-    String(dateFns.addSeconds(new Date(0), seconds)),
+    String(dateFns.addSeconds(new Date(UNIX_TIME_START), secondsAsNumber)),
     DateFormatType.HOURS_MINUTES_SECONDS
   )
 
   const splittedTime = timeInFormatHHmmss.split(':')
-  let time = splittedTime[0] + ':' + splittedTime[1] + ':' + splittedTime[2]
-  if (splittedTime[0] === '00') {
-    time = splittedTime[1] + ':' + splittedTime[2]
+  const hours = splittedTime[0]
+  const minutes = splittedTime[1]
+  const seconds = splittedTime[2]
+
+  const isHourExist = checkHourExist(hours)
+  let time = minutes + ':' + seconds
+  if (isHourExist) {
+    time = hours + ':' + minutes + ':' + seconds
   }
+
   return time
 }
 
