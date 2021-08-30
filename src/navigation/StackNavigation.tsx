20144e43dae6bb6f3bf581218d14c6a550a247c4
Yevhenii <eucheremisin@gmail.com>
2021-08-30 10:29:49 +0300

    J-286: +new-files-w-upper-case

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationScreen } from 'src/common/enums'
+import { SignIn } from 'src/screens'
+import TabNavigation from './TabNavigation'
+
+const Stack = createStackNavigator()
+
+const StackNavigation: React.FC = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName={NavigationScreen.SIGN_IN}
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#fff' }
+      }}
+    >
+      <Stack.Screen name={NavigationScreen.SIGN_IN} component={SignIn} />
+      <Stack.Screen
+        name={NavigationScreen.TABS_NAV}
+        component={TabNavigation}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default StackNavigation
