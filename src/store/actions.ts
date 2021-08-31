ad21744a1afd6c9ef035751b7353267555520684
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-31 23:40:56 +0300

    J-302: + 'Home' reducer

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,1 +1,5 @@
 export { signIn, getCurrentUser, resetUser } from './auth/actions'
+export {
+  loadSuggestedPodcasts,
+  loadRecentlyPlayedEpisodes
+} from './home/actions'
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import { reducer as authReducer } from './auth'
+import { reducer as homeReducer } from './home'
 
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  home: homeReducer
 })
 
 export default rootReducer
