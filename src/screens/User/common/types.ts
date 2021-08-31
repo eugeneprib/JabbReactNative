6d53229d842cc84d2c481d97d5e608a399eac540
Yevhenii <eucheremisin@gmail.com>
2021-08-31 19:19:24 +0300

    J-309: +types-and-imports

---
+++ src/screens/User/common/types.ts
@@ -0,0 +1,15 @@
+type Podcast = {
+  name: string
+  author: string
+  image: string
+}
+
+type User = {
+  firstname: string
+  surname: string
+  nickname: string
+  email: string
+  image: string
+}
+
+export type { Podcast, User }
