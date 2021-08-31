d194b95a4d1510f2ddca36e5d2e14f8495227d69
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-08-31 23:34:21 +0300

    J-302: + AsyncStorage

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,19 +1,24 @@
 import EncryptedStorage from 'react-native-encrypted-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { ENV } from 'src/common/enums'
 import { Http } from './http'
 import { AuthApi } from './authApi'
-import { Storage } from './storage'
-import { UserApi } from './userApi'
 import { Notification } from './notification'
+import { Storage } from './storage'
+import { PodcastApi } from './podcastApi'
 
 const notification = new Notification()
 
 const storage = new Storage({
+  storage: AsyncStorage
+})
+
+const secureStorage = new Storage({
   storage: EncryptedStorage
 })
 
 const http = new Http({
-  storage
+  storage: secureStorage
 })
 
 const authApi = new AuthApi({
@@ -21,9 +26,9 @@ const authApi = new AuthApi({
   apiPath: ENV.API_PATH
 })
 
-const userApi = new UserApi({
+const podcastApi = new PodcastApi({
   http,
   apiPath: ENV.API_PATH
 })
 
-export { authApi, storage, userApi, notification }
+export { notification, storage, secureStorage, authApi, podcastApi }
