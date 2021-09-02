28c80f833db272712fa39fdc2f9b5be5ac4f3a10
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-09-02 20:33:46 +0300

    J-331: * 'Home' component

--- src/screens/Home/components/RecentlyPlayedCard/RecentlyPlayedCard.tsx
+++ src/screens/Home/components/RecentlyPlayedCard/RecentlyPlayedCard.tsx
@@ -1,32 +1,41 @@
 import React from 'react'
 import { Image, TouchableOpacity, View, ViewStyle } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import { Heading, HeadingType, PlainText } from 'src/components'
 import { getFormattedDate, DateFormatType } from 'src/helpers'
+import { NavigationScreen } from 'src/common/enums'
+import { HomeScreenNavigationProp } from 'src/screens/Home/common/types'
 import TimeIcon from 'src/assets/images/time.svg'
 import styles from './styles'
 
 type Props = {
+  id: number
   title: string
   author: string
   date: string
   source?: string
   style?: ViewStyle
-  onPress?: () => void
 }
 
 const RecentlyPlayedCard: React.FC<Props> = ({
+  id,
   title,
   author,
   date,
   source,
-  style,
-  onPress
+  style
 }) => {
+  const navigation = useNavigation<HomeScreenNavigationProp>()
+
+  const handlePress = () => {
+    navigation.navigate(NavigationScreen.EPISODE, { id })
+  }
+
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       style={[styles.container, style]}
-      onPress={onPress}
+      onPress={handlePress}
     >
       <Image source={{ uri: source }} style={styles.image} />
       <View>
