0a42a9d8679117744278c73d42148f2bcf2240e9
Denys Lunhu <60543500+Denidzio@users.noreply.github.com>
2021-09-07 12:21:28 +0300

    J-384: + 'ProfileScreenNavigationProp' type

--- src/screens/Profile/common/types/profileScreenNavigationProp.ts
+++ src/screens/Profile/common/types/profileScreenNavigationProp.ts
@@ -2,9 +2,9 @@ import { StackNavigationProp } from '@react-navigation/stack'
 import { NavigationScreen } from 'src/common/enums'
 import { RootStackParamList } from 'src/common/types'
 
-type UserScreenNavigationProp = StackNavigationProp<
+type ProfileScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   NavigationScreen.MY_PROFILE
 >
 
-export type { UserScreenNavigationProp }
+export type { ProfileScreenNavigationProp }
