cda3001b092b51cc3dfc154be92275fafef30fa3
eugeneprib <yevgenyprib@gmail.com>
2021-09-08 12:21:45 +0300

    Fix/Player duration time

--- src/screens/Episode/components/Player/common/helpers/getPlayerTime.ts
+++ src/screens/Episode/components/Player/common/helpers/getPlayerTime.ts
@@ -1,30 +1,24 @@
-import { DateFormatType, getFormattedDate } from 'src/helpers'
 import {
   TIME_SEPARATOR,
   HOUR_IS_NOT_EXISTS,
   MILLISECONDS_IN_SECOND
 } from '../constants'
 
-const checkHourExist = (hours: string): boolean => {
-  return hours !== HOUR_IS_NOT_EXISTS
-}
-
 const getPlayerTime = (timestamp: number): string => {
-  const formattedTime = getFormattedDate(
-    String(new Date(timestamp * MILLISECONDS_IN_SECOND)),
-    DateFormatType.HOURS_MINUTES_SECONDS
-  )
+  const time = new Date(timestamp * MILLISECONDS_IN_SECOND)
+    .toISOString()
+    .slice(-13, -5)
 
-  const splittedTime = formattedTime.split(TIME_SEPARATOR)
+  const splittedTime = time.split(TIME_SEPARATOR)
   const [hours, minutes, seconds] = splittedTime
 
-  const minutesAndSeconds = minutes + TIME_SEPARATOR + seconds
+  let formattedTime = `${minutes}:${seconds}`
 
-  if (!checkHourExist(hours)) {
-    return minutesAndSeconds
+  if (hours !== HOUR_IS_NOT_EXISTS) {
+    formattedTime = `${hours}:${formattedTime}`
   }
 
-  return hours + TIME_SEPARATOR + minutesAndSeconds
+  return formattedTime
 }
 
 export { getPlayerTime }
