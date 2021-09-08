984293488916909cb8b9307832b8c708310d3bb9
Yevhenii <eucheremisin@gmail.com>
2021-09-08 13:38:57 +0300

    J-386: *background-for-tab-navigation

--- src/navigation/components/TabNavigation/options/index.ts
+++ src/navigation/components/TabNavigation/options/index.ts
@@ -2,7 +2,7 @@ export const tabOptions = {
   headerShown: false,
   tabBarShowLabel: false,
   tabBarStyle: {
-    backgroundColor: 'rgb(250,250,250)',
+    backgroundColor: '#fff',
     height: 58,
     paddingHorizontal: 18
   }
